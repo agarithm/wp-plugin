919536927534c861954ae3d3ab78a3690d413e07
Mike Agar <mike@agarithm.com>
2020-07-09 12:00:32 +0000

    Now with a non-conflicting namespace

---
+++ sample-plugin/includes/class-sample-plugin-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Sample_Plugin
+ * @subpackage Sample_Plugin/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Sample_Plugin
+ * @subpackage Sample_Plugin/includes
+ * @author     Your Name <email@example.com>
+ */
+class Sample_Plugin_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
